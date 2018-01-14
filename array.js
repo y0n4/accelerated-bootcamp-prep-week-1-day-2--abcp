d87295919cff9789148238810dc6702b242db4c6
yona l <04l.yona@gmail.com>
2018-01-14 18:11:51 +0000

    Done.

--- array.js
+++ array.js
@@ -28,12 +28,14 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
+  return array.slice(1);
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-
+  array.pop();
+  return array;
 }
 
 function removeElementFromEndOfArray(array) {
-
+  return array.slice(0, array.length-1);
 }
