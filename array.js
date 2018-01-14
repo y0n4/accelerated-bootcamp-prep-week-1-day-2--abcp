b65d8abbcd315ec5b6e00c5b65637925024d4bf5
yona l <04l.yona@gmail.com>
2018-01-14 17:42:41 +0000

    Done.

--- array.js
+++ array.js
@@ -1,25 +1,30 @@
 function addElementToBeginningOfArray(array, element) {
  // add the element to the beginning of the array without mutating the array
+ return [element, ...array];
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
   // add element to the beginning of the array by mutating the array
+  array.unshift(element);
+  return array;
 }
 
 function addElementToEndOfArray(array, element) {
-
+  return [array, ...element];
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-
+  array.push(element);
+  return array;
 }
 
 function accessElementInArray(array, index) {
-
+  return array[index];
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-
+  array.shift();
+  return array;
 }
 
 function removeElementFromBeginningOfArray(array) {
